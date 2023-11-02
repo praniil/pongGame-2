a0c50b0101982ecf581cc30a2763bf92ab1582ae
praniil <pparajuli15@gmail.com>
2023-11-02 13:06:56 +0545

    feat: updated the game

--- game.cpp
+++ game.cpp
@@ -7,7 +7,7 @@ using namespace sf;
 Game ::Game()
 {
     game_backgroundColor = Color ::Black;
-    game_paddleColor = Color ::Green;
+    game_paddleColor = Color ::White;
     game_ballColor = Color ::White;
     game_playerPaddle.setSize(Vector2f(25, 75));
     game_cpuPaddle.setSize(Vector2f(25, 75));
@@ -15,7 +15,6 @@ Game ::Game()
     game_ballVelocityX = 450;
     game_ballVelocityY = 450;
     game_paddleVelocity = 7;
-    
 
     // game window
     game_window.create(VideoMode(1280, 800), "Pong Game");
@@ -34,21 +33,31 @@ void Game ::run()
 {
     while (game_window.isOpen())
     {
+        Event event;
+        while (game_window.pollEvent(event))
+        {
+            if (event.type == Event ::Closed)
+            {
+                game_window.close();
+            }
+        }
         Time time = game_clock.restart();
         update(time);
         render();
     }
 }
-void Game::update(sf::Time time) {
-    if (Keyboard::isKeyPressed(Keyboard::Up) && game_playerPaddle.getPosition().y > 0) {
+void Game::update(sf::Time time)
+{
+    if (Keyboard::isKeyPressed(Keyboard::Up) && game_playerPaddle.getPosition().y > 0)
+    {
         game_playerPaddle.move(0, -450 * time.asSeconds());
     }
-    if (Keyboard::isKeyPressed(Keyboard::Down) && game_playerPaddle.getPosition().y < game_window.getSize().y - game_playerPaddle.getSize().y) {
+    if (Keyboard::isKeyPressed(Keyboard::Down) && game_playerPaddle.getPosition().y < game_window.getSize().y - game_playerPaddle.getSize().y)
+    {
         game_playerPaddle.move(0, 450 * time.asSeconds());
     }
     game_ball.move(game_ballVelocityX * time.asSeconds(), game_ballVelocityY * time.asSeconds());
 }
-
 
 void Game ::render()
 {
